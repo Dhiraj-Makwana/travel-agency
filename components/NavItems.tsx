10e4b65f547027307daaf38c6c601f2426849703
Dhiraj-Makwana <dhirajmakwana33912@gmail.com>
2025-07-25 15:49:59 +0530

    Auth UI, Enterprise Error Tracking & monitoring, Users table, Add new trip form, AI trip generation

--- components/NavItems.tsx
+++ components/NavItems.tsx
@@ -1,13 +1,17 @@
-import { Link, NavLink } from 'react-router';
+import {Link, NavLink, useLoaderData, useNavigate} from 'react-router';
 import {sidebarItems} from "~/constants";
 import {cn} from "~/lib/utils";
+import {logoutUser} from "~/appwrite/auth";
 
 const NavItems = ({ handleClick }: { handleClick?: () => void}) => {
-    const user = {
-        name: 'Adrian',
-        email: 'contact@jsmastery.pro',
-        imageUrl: '/assets/images/david.webp'
+    const user = useLoaderData();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        await logoutUser();
+        navigate('sign-in')
     }
+
     return (
         <section className='nav-items'>
             <Link to='/' className="link-logo" >
@@ -36,7 +40,7 @@ const NavItems = ({ handleClick }: { handleClick?: () => void}) => {
                 </nav>
 
                 <footer className="nav-footer">
-                    <img src={user?.imageUrl || '/assets/images/david.webp'} alt={user?.name || 'David'} />
+                    <img src={user?.imageUrl || '/assets/images/david.webp'} alt={user?.name || 'David'} referrerPolicy={'no-referrer'} />
 
                     <article>
                         <h2>{user?.name}</h2>
@@ -44,9 +48,7 @@ const NavItems = ({ handleClick }: { handleClick?: () => void}) => {
                     </article>
 
                     <button
-                        onClick={() => {
-                            console.log('logout');
-                        }}
+                        onClick={handleLogout}
                         className="cursor-pointer"
                     >
                         <img
